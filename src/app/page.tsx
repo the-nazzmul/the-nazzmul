070951550e758a0d54145e935fa27022dfa44050
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-26 17:48:32 +0600

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeaderSection from "@/components/sections/header";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <HeaderSection />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,9 @@ export default {
       },
     },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        serif: ["var(--font-serif)"],
       },
     },
   },
