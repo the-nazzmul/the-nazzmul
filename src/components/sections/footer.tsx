ca29acc82c8169b008d0b307b02ff23021e13444
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-03-04 15:12:30 +0600

    made adjustment for tablet and desktop. Time for animations

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -21,9 +21,9 @@ const Footer = () => {
     <footer className="relative z-0 overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10 pointer-events-none" />
       <div className="container">
-        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row items-center gap-8 justify-between">
           <div className="text-white/40">&copy; 2025. All rights reserved.</div>
-          <nav className="flex flex-col items-center gap-8">
+          <nav className="flex flex-col items-center gap-8 md:flex-row">
             {FOOTER_LINKS.map((link) => (
               <Link
                 key={link.title}
