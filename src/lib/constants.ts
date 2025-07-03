24a3a52ab8e558e9ef27f5242a63173f842008d4
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-07-03 20:01:25 +0600

    added project details

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,10 +3,9 @@ export const PROJECTS_DATA = [
     title: "Mireon",
     type: "full-stack",
     year: 2025,
-    overview:
-      "Gemini powered github management tool to help with code review, explanation, debugging and improvement.",
+    overview: "AI-powered code intelligence and meeting analysis platform.",
     description:
-      "Mireon is a Gemini powered github management tool to help with code review, explanation, debugging and improvement. It can load any github repo, create embedding for each files and stores them to the database. It can also generate a summary of the code and provide a chatbot to answer questions about the code. It can also point out the issues in the code and provide suggestions to improve the code. It also has team collaboration features. It can also process meeting audio with Assembly AI and generate a summary of the meeting.",
+      "Mireon is an AI-powered code intelligence and meeting analysis platform. It enables teams to index, understand, and interact with their GitHub repositories and meetings through natural language. Whether it's code review, onboarding, or documentation, Mireon helps you navigate complex projects faster.",
     challenges: [
       {
         title:
@@ -43,59 +42,11 @@ export const PROJECTS_DATA = [
       { title: "PostgreSQL" },
       { title: "Gemini" },
       { title: "Assembly AI" },
+      { title: "Langchain JS" },
+      { title: "Stripe" },
     ],
     image:
-      "https://ghks3sgigz.ufs.sh/f/sJdwCCpG4fmU9Q9MqJmvTSLkYxuBEfwHdGVZNPlRQhmr7pKi",
-    github: "https://github.com/the-nazzmul/mireon",
-    livesite: "https://mireon-nazz.vercel.app",
-  },
-  {
-    title: "DevView",
-    type: "full-stack",
-    year: 2025,
-    overview:
-      "Gemini powered github management tool to help with code review, explanation, debugging and improvement.",
-    description:
-      "Mireon is a Gemini powered github management tool to help with code review, explanation, debugging and improvement. It can load any github repo, create embedding for each files and stores them to the database. It can also generate a summary of the code and provide a chatbot to answer questions about the code. It can also point out the issues in the code and provide suggestions to improve the code. It also has team collaboration features. It can also process meeting audio with Assembly AI and generate a summary of the meeting.",
-    challenges: [
-      {
-        title:
-          "Struggles with processing big projects on Vercel because of timeout",
-      },
-      {
-        title:
-          "Sequential indexing for staying under the rate limit of Gemini's free tier",
-      },
-      { title: "Vercel's max duration limitation" },
-      { title: "Gemini's free tier limitation" },
-    ],
-    improvements_scope: [
-      { title: "Transaction and rollback implementation" },
-      { title: "Better error handling" },
-      { title: "Caching" },
-      { title: "UI improvements" },
-    ],
-    mainFeatures: [
-      { title: "Code embedding" },
-      { title: "Code summarization" },
-      { title: "Code chatbot" },
-      { title: "Code issues" },
-      { title: "Code suggestions" },
-      { title: "Team collaboration" },
-      { title: "Meeting summarization" },
-    ],
-    technologies: [
-      { title: "Next.js" },
-      { title: "TypeScript" },
-      { title: "Tailwind CSS" },
-      { title: "Clerk" },
-      { title: "Prisma" },
-      { title: "PostgreSQL" },
-      { title: "Gemini" },
-      { title: "Assembly AI" },
-    ],
-    image:
-      "https://ghks3sgigz.ufs.sh/f/sJdwCCpG4fmU9Q9MqJmvTSLkYxuBEfwHdGVZNPlRQhmr7pKi",
+      "https://875p1967n4.ufs.sh/f/l33HWhV9rVqMT1uLc97zxFLVGd48vuJS06bMR7aZoPYHBjWc",
     github: "https://github.com/the-nazzmul/mireon",
     livesite: "https://mireon-nazz.vercel.app",
   },
@@ -104,13 +55,12 @@ export const PROJECTS_DATA = [
     type: "full-stack",
     year: 2025,
     overview:
-      "Gemini powered github management tool to help with code review, explanation, debugging and improvement.",
+      "AI-powered PDF research assistant designed to help users extract meaningful insights from their PDFs.",
     description:
-      "Mireon is a Gemini powered github management tool to help with code review, explanation, debugging and improvement. It can load any github repo, create embedding for each files and stores them to the database. It can also generate a summary of the code and provide a chatbot to answer questions about the code. It can also point out the issues in the code and provide suggestions to improve the code. It also has team collaboration features. It can also process meeting audio with Assembly AI and generate a summary of the meeting.",
+      "Savant is an AI-powered PDF research assistant designed to help users extract meaningful insights from their PDFs. Built on Google's Gemini AI, Savant enables users to upload documents, chat with them, and even analyze resumes for ATS compatibility.",
     challenges: [
       {
-        title:
-          "Struggles with processing big projects on Vercel because of timeout",
+        title: "Struggles with processing big PDF on Vercel because of timeout",
       },
       {
         title:
@@ -124,15 +74,13 @@ export const PROJECTS_DATA = [
       { title: "Better error handling" },
       { title: "Caching" },
       { title: "UI improvements" },
+      { title: "File Management System implementation" },
     ],
     mainFeatures: [
-      { title: "Code embedding" },
-      { title: "Code summarization" },
-      { title: "Code chatbot" },
-      { title: "Code issues" },
-      { title: "Code suggestions" },
-      { title: "Team collaboration" },
-      { title: "Meeting summarization" },
+      { title: "Intelligent interaction with resume" },
+      { title: "Vectorized search" },
+      { title: "Chatting in the context of the document" },
+      { title: "Resume analysis enhanced" },
     ],
     technologies: [
       { title: "Next.js" },
@@ -141,13 +89,53 @@ export const PROJECTS_DATA = [
       { title: "Clerk" },
       { title: "Prisma" },
       { title: "PostgreSQL" },
+      { title: "PineconeDB" },
       { title: "Gemini" },
-      { title: "Assembly AI" },
+      { title: "Langchain JS" },
     ],
     image:
-      "https://ghks3sgigz.ufs.sh/f/sJdwCCpG4fmU9Q9MqJmvTSLkYxuBEfwHdGVZNPlRQhmr7pKi",
-    github: "https://github.com/the-nazzmul/mireon",
-    livesite: "https://mireon-nazz.vercel.app",
+      "https://875p1967n4.ufs.sh/f/l33HWhV9rVqMUCWa385VqDOyRwr6g1FhnWLEvTlo8msPGzCi",
+    github: "https://github.com/the-nazzmul/savant",
+    livesite: "https://savant-nazz.vercel.app/",
+  },
+  {
+    title: "DevView",
+    type: "full-stack",
+    year: 2025,
+    overview: "Custom remote interview platform for developers.",
+    description:
+      "DevView is a remote interview platform for developers. Interviewers can schedule, conduct, and review interviews, while candidates can solve coding problems in a Monaco-powered editor. Built with Next.js, Convex, Clerk, and Stream SDK.",
+    challenges: [
+      {
+        title: "The code editor and question bank is not dynamic",
+      },
+
+      { title: "Some issues with hardware access" },
+    ],
+    improvements_scope: [
+      {
+        title: "The code editor needs to be interactive",
+      },
+      { title: "A custom backend server" },
+      { title: "Interactivity" },
+    ],
+    mainFeatures: [
+      { title: "Video and audio calling" },
+      { title: "Web based code editor and question bank" },
+      { title: "Scheduling and feedback system" },
+    ],
+    technologies: [
+      { title: "Next.js" },
+      { title: "TypeScript" },
+      { title: "Tailwind CSS" },
+      { title: "Clerk" },
+      { title: "Convex" },
+      { title: "Stream SDK" },
+    ],
+    image:
+      "https://875p1967n4.ufs.sh/f/l33HWhV9rVqMDwijyLEmpOxItRnsgj9ZMu6KmPJo2BTldW1N",
+    github: "https://github.com/the-nazzmul/dev-view",
+    livesite: "https://dev-view.vercel.app/",
   },
 ];
 
